056ccb1595b38e5977f1d0a2b1fbfc3e2370edd4
roberto08ws <roberto.henrique.08.ws@gmail.com>
2024-10-23 09:18:13 -0300

    Variáveis criadas email e senha

--- app/src/main/java/com/example/myapp4/MainActivity.java
+++ app/src/main/java/com/example/myapp4/MainActivity.java
@@ -26,5 +26,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        view.btnEntrar.setOnClickListener(e -> {
+
+            String email = view.edtEmail.getText().toString().trim();
+            String senha = view.edtSenha.getText().toString().trim();
+
+        });
+
     }
 }
