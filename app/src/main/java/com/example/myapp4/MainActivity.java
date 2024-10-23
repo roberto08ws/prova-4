05cc3366e542e9c56ae346509daaeec9254bd028
roberto08ws <roberto.henrique.08.ws@gmail.com>
2024-10-23 09:17:17 -0300

    ICriação da variável da view da activity MainActivity(Tela login)

--- app/src/main/java/com/example/myapp4/MainActivity.java
+++ app/src/main/java/com/example/myapp4/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapp4;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,13 +9,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.myapp4.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        view = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(view.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
