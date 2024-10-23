c689deb506be284e90127cb22ed816283801549d
roberto08ws <roberto.henrique.08.ws@gmail.com>
2024-10-23 09:56:46 -0300

    Variável do layout da activity TelaDisciplinas criada

--- app/src/main/java/com/example/myapp4/TelaDisciplinas.java
+++ app/src/main/java/com/example/myapp4/TelaDisciplinas.java
@@ -1,5 +1,6 @@
 package com.example.myapp4;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.myapp4.databinding.ActivityTelaDisciplinasBinding;
+
 public class TelaDisciplinas extends AppCompatActivity {
+
+    private ActivityTelaDisciplinasBinding view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_tela_disciplinas);
+        view = ActivityTelaDisciplinasBinding.inflate(getLayoutInflater());
+        setContentView(view.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
     }
 }
